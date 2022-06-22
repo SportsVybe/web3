f4ebac0b43b31093d5c2321c8b2bc02659a964cd
Avrahm <avrahmk@me.com>
2022-06-22 06:52:07 -0400

    abi, vtoken, and constants

---
+++ client/configs/constants.ts
@@ -0,0 +1,30 @@
+export const contractFunctions = {
+  createTeam: "createTeam",
+  declineChallenge: "declineChallenge",
+  acceptChallenge: "acceptChallenge",
+  createChallengePool: "createChallengePool",
+  sendTeamMembershipRequest: "sendTeamMembershipRequest",
+  acceptMembershipTeamRequest: "acceptMembershipTeamRequest",
+  getTeamMate: "getTeamMate",
+  getTeamCount: "getTeamCount",
+  joinChallengePool: "joinChallengePool",
+  getTeamSportsmanship: "getTeamSportsmanship",
+};
+
+export const contractVariables = {
+  teamCount: "teamCount",
+  teamSportsmanship: "team_sportsmanship",
+  teamOwner: "team_owner",
+  teamMembers: "teamMembers",
+  teamMembershipRequests: "team_membership_request",
+  ChallengePool: "ChallengePool",
+};
+
+export const sports = [
+  "Basketball",
+  "Baseball",
+  "Football",
+  "Hockey",
+  "Soccer",
+  "Tennis",
+];
